3572a5ce1fe2a69ec2a0f7825bcbb5d12e7e606e
sander1 <sander@plexapp.com>
2013-03-28 04:48:32 +0100

    Log a message if username/password aren't set and when a search fails or gives 0 results.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -15,41 +15,48 @@ class MALAgentTV(Agent.TV_Shows):
 
 	def search(self, results, media, lang):
 
-		if media.show.endswith(' Ova'):
-			media.show = '%s (OVA)' % media.show.rsplit(' ', 1)[0]
-		elif media.show.endswith(' Tv'):
-			media.show = '%s (TV)' % media.show.rsplit(' ', 1)[0]
-
-		Log("[MyAnimeList Agent]: Search for Anime: %s" % media.show)
-
 		if Prefs['username'] and Prefs['password']:
+
+			if media.show.endswith(' Ova'):
+				media.show = '%s (OVA)' % media.show.rsplit(' ', 1)[0]
+			elif media.show.endswith(' Tv'):
+				media.show = '%s (TV)' % media.show.rsplit(' ', 1)[0]
+
+			Log("[MyAnimeList Agent]: Search for Anime: %s" % media.show)
+
 			#create url with name of anime
 			mal_url = SEARCH_URL % media.show.replace(' ', '+')
 
-			#get the xml dom from adress with authorization
-			xml = XML.ElementFromURL(mal_url, headers=createAuthorizationHeader())
+			try:
+				#get the xml dom from adress with authorization
+				xml = XML.ElementFromURL(mal_url, headers=createAuthorizationHeader())
 
-			#add data to results
-			for entry in xml.xpath('//entry'):
-				id = entry.xpath('./id/text()')[0]
-				name = entry.xpath('./title/text()')[0]
+				#add data to results
+				for entry in xml.xpath('//entry'):
+					id = entry.xpath('./id/text()')[0]
+					name = entry.xpath('./title/text()')[0]
 
-				try:
-					year = entry.xpath('./start_date/text()')[0].split('-')[0]
-				except:
-					year = None
+					try:
+						year = entry.xpath('./start_date/text()')[0].split('-')[0]
+					except:
+						year = None
 
-				score = 100 - abs(String.LevenshteinDistance(name, media.show))
+					score = 100 - abs(String.LevenshteinDistance(name, media.show))
 
-				results.Append(MetadataSearchResult(
-					id = id,
-					name = name,
-					year = year,
-					score = score,
-					lang = Locale.Language.English
-				))
+					results.Append(MetadataSearchResult(
+						id = id,
+						name = name,
+						year = year,
+						score = score,
+						lang = Locale.Language.English
+					))
+			except:
+				Log("[MyAnimeList Agent]: Error in retrieving xml data, or 0 results for search query for: %s" % media.show)
+		else:
+			Log("[MyAnimeList Agent]: Username/password not set")
 
 	def update(self, metadata, media, lang):
+
 		Log("[MyAnimeList Agent]: Update Anime-data: %s" % media.title)
 
 		#create the url with the name
@@ -70,4 +77,5 @@ class MALAgentTV(Agent.TV_Shows):
 			if item.tag == 'start_date':
 				metadata.originally_available_at = Datetime.ParseDate(item.text)
 			if item.tag == 'image':
-				metadata.posters[item.text] = Proxy.Media(HTTP.Request(item.text).content)
+				if item.text not in metadata.posters:
+					metadata.posters[item.text] = Proxy.Media(HTTP.Request(item.text).content)
